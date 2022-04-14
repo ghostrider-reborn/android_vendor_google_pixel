43f9688dc702b3a48053db860e2a832d40429fec
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-15 05:01:53 +0530

    pixel: Duplicate DocumentsUI overlay for AOSP
     * this should cover for builds with and without partner_modules

--- config.mk
+++ config.mk
@@ -18,6 +18,7 @@ $(call inherit-product-if-exists, vendor/google/pixel/pixel-vendor.mk)
 # Overlays
 PRODUCT_PACKAGES += \
     PixelDocumentsUIOverlay \
+    PixelDocumentsUIGoogleOverlay \
     PixelFrameworksOverlay \
     PixelSettingsOverlay \
     PixelSystemUIOverlay
